5e2d26fcbedc97061b65d77d41661c14d9f988a7
SupremYerd <andrey.zeferino@outlook.com>
2023-10-29 19:11:24 -0300

    connection class

--- controller/connection.php
+++ controller/connection.php
@@ -1,17 +1,29 @@
 <?php
+class Connection {
+    private $server = 'localhost';
+    private $user = 'root';
+    private $pass = '123456';
+    private $db = 'world';
+    private $conn;
 
-$server = 'localhost';
-$user = 'root';
-$pass = '123456';
-$db = 'world';
+    public function __construct() {
+        try {
+            $this->conn = new PDO("myshql:host=$this->server;dbname=$this->db", $this->user, $this->pass);
+            echo "conectado a $this->server $this->db Com sucesso usando pdo";
+        } catch (PDOException $e) {
+            $message = "Drivers disponiveis: " . implode(',', PDO::getAvailableDrivers());
+            $message .= "\n -Erro: " . $e->getMessage();
+            throw new Exception($message);
+        }
+    }
 
-try {
-    $conn = new PDO("myshql:host=$server;dbname=$db", $user, $pass);
-    echo "conectado a $server $db Com sucesso usando pdo";
-} catch (PDOException $e) {
-    $message = "Drivers disponiveis: " . implode(',', PDO::getAvailableDrivers());
-    $message .= "\n -Erro: " . $e->getMessage();
-    throw new Exception($message);
+    public function query(string $sql) {
+        try {
+            return $this->conn->query($sql);
+        } catch (PDOException) {
+
+        }
+    }
 }
 
 ?>
--- models/Cliente.php
+++ models/Cliente.php
@@ -1,4 +1,5 @@
 <?php
+
 class Cliente {
     
     private int $id;
