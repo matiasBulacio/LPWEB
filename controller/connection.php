21a9c3e54820af4524f0f54518ae967c98859bd2
SupremYerd <andrey.zeferino@outlook.com>
2023-11-12 14:39:24 -0300

    create para cliente e produto

--- controller/connection.php
+++ controller/connection.php
@@ -1,29 +1,16 @@
 <?php
-class Connection {
-    private $server = 'localhost';
-    private $user = 'root';
-    private $pass = '123456';
-    private $db = 'world';
-    private $conn;
+$server = 'localhost';
+$user = 'root';
+$pass = '';
+$db = 'bd_aulas';
+$conn;
 
-    public function __construct() {
-        try {
-            $this->conn = new PDO("myshql:host=$this->server;dbname=$this->db", $this->user, $this->pass);
-            echo "conectado a $this->server $this->db Com sucesso usando pdo";
-        } catch (PDOException $e) {
-            $message = "Drivers disponiveis: " . implode(',', PDO::getAvailableDrivers());
-            $message .= "\n -Erro: " . $e->getMessage();
-            throw new Exception($message);
-        }
-    }
 
-    public function query(string $sql) {
-        try {
-            return $this->conn->query($sql);
-        } catch (PDOException) {
-
-        }
-    }
+try {
+    $conn = new PDO("mysql:host=$server;dbname=$db", $user, $pass);
+    echo "conectado a $server $db Com sucesso usando pdo";
+} catch (PDOException $e) {
+    $message = "Drivers disponiveis: " . implode(',', PDO::getAvailableDrivers());
+    $message .= "\n -Erro: " . $e->getMessage();
+    throw new Exception($message);
 }
-
-?>
